63f9a261f052a0ade188770d04f4edf4d9208d7e
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2023-12-20 22:36:49 +0200

    completed todo app in one component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,81 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Every time state changed component will be render again
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  // will enter infite loop 
+  // every time value of state change component will render again
+  // setNewItem("fffffff")
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (newItem == "") return;
+    // will keep only last value added 
+    // always start from empty array on useState
+    // setTodos([...todos, {
+    //   id: crypto.randomUUID(),
+    //   title: newItem,
+    //   completed: false
+    // }])
+
+    // To save current todos and append to it
+    // we use arrow function with current value
+    setTodos((currentTodos) => [...currentTodos, {
+      id: crypto.randomUUID(),
+      title: newItem,
+      completed: false
+    }])
+
+    // cleant input item
+    setNewItem("")
+  }
+
+  function toggleTodo(id, checked) {
+    setTodos((currentTodos) => {
+      return currentTodos.map(todo => {
+        if (todo.id == id) {
+          return { ...todo, completed: checked }
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+            type="text" id='item' />
+        </div>
+        <button className='btn'>Add</button>
+      </form>
+      <h1 className='header'>Todo List</h1>
+      <ul className='list'>
+        {todos.length === 0 && " No Todos"}
+        {todos.map(todo => {
+          // we set key for each li because react use it internaly to update or change any element
+          return <li key={todo.id}>
+            <label>
+              <input onChange={e => toggleTodo(todo.id, e.target.checked)} type="checkbox" checked={todo.completed} />
+              {todo.title}
+            </label>
+            <button onClick={() => deleteTodo(todo.id)} className='btn btn-danger'>Delete</button>
+          </li>
+        })}
+
+      </ul>
     </>
   )
 }
