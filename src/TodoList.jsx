246534a4b1c4c8602bdb7160cc95eb99f903c9dd
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2023-12-20 23:00:56 +0200

    split todo list into muliple todoitem compenets

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,16 +1,12 @@
+import { TodoItem } from "./TodoItem"
+
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
 
     return <ul className='list'>
         {todos.length === 0 && " No Todos"}
         {todos.map(todo => {
             // we set key for each li because react use it internaly to update or change any element
-            return <li key={todo.id}>
-                <label>
-                    <input onChange={e => toggleTodo(todo.id, e.target.checked)} type="checkbox" checked={todo.completed} />
-                    {todo.title}
-                </label>
-                <button onClick={() => deleteTodo(todo.id)} className='btn btn-danger'>Delete</button>
-            </li>
+            return <TodoItem toggleTodo={toggleTodo} deleteTodo={deleteTodo} {...todo} key={todo.id} />
         })}
 
     </ul>
